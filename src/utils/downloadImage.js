c173e363ae24b80991b31afcb7d466e38f6f9830
N1hron <denik8008@gmail.com>
2024-02-10 23:31:14 +0300

    add dot removal from image name on download

--- src/utils/downloadImage.js
+++ src/utils/downloadImage.js
@@ -1,7 +1,7 @@
 export default function downloadImage(image, imageName = '') {
     const a = document.createElement('a')
 
-    a.href = image.src
-    a.setAttribute('download', imageName)
+    a.href = image.src; console.log(image)
+    a.setAttribute('download', imageName.replace('.', ''))
     a.click()
 }
